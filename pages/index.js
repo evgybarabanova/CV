14f9cef86741ba6e5bccf5e004e1e287edcb2d55
Evgy <evgybarabanova@gmail.com>
2022-09-08 23:53:44 +0300

    correct texts in landing page

--- pages/index.js
+++ pages/index.js
@@ -6,27 +6,15 @@ import MainLayout from '../components/MainLayout'
 export default function Index() {
   return (
     <MainLayout title='Main Page'>
-      <>
-        <h1>aaaaaaaaaaaaaa</h1>
-        {/* <img src={pic} alt='pic' /> */}
-        <h1>HI! I`M EVGENIA BARABANOVA</h1>
-        <p>FULL STACK DEVELOPER</p>
-        <p>Hello! I`m Evgenia Barabanova, full stack developer with 1 over of expirience.
-          I am able to create a web application
-          from scratch both client-side and server-side.
-        </p>
-        <div className='container'>
-          <p>Age: 31</p>
-          <p>Location: Moscow</p>
-          <p>email: evgybarabanova@gmail.com</p>
-          <p>phone: +7 927 190 63 92</p>
-          <p>Website: </p>
-          <p>Social Link</p>
-          <button>whats app</button>
-          <button>Telegram</button>
-          <button>linkedin</button>
-        </div>
-      </>
+      <h1>aaaaaaaaaaaaaa</h1>
+
+      <h1>Hello World! I`m EVGENIA BARABANOVA</h1>
+      <p>I'm a FULL STACK WEB DEVELOPER</p>
+      <p>With 1 year of experience,
+        I'm able to create web applications
+        from scratch, both in client and server side.
+      </p>
+      <p><Link href="/contact">Contact me!</Link></p>
     </MainLayout>
 
   )
